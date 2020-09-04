6c800af03487482ebd68f2b277cd2573158a7489
Johannes Englisch <cyberjoe0815@hotmail.com>
2020-09-04 10:03:57 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_ditransitive',
+    py_modules=['cldfbench_ditransitive'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'ditransitive=cldfbench_ditransitive:Dataset',
+        ]
+    },
+    install_requires=[
+        'ditrans2cldf',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
