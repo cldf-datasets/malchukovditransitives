c3af896d0917f3a2a1c536be77bbd79941b1b9f5
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-08-27 12:17:01 +0200

    set up pyproject.toml

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='cldfbench_ditransitive',
-    py_modules=['cldfbench_ditransitive'],
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'cldfbench.dataset': [
-            'ditransitive=cldfbench_ditransitive:Dataset',
-        ]
-    },
-    install_requires=[
-        'ditrans2cldf',
-        'cldfbench[glottolog,excel]',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
-)
+setup()
